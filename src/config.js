744cae6051554beff8c97f8d02ab2b799c1518aa
unknown <yalin.jing@zdjt.com>
2026-05-28 00:36:50 +0800

    chore: update Coze API token

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // 扣子 API 配置 - 请在此填入你的 PAT 和 Bot ID
 const COZE_BASE = 'https://api.coze.cn';
 export const COZE_CONFIG = {
-  PAT: 'pat_7DOVYxserBFUi3roSBysUK0n3h5XJhzyXHUsvpWNWIhkoaEqecMrtgYzG8jRbVwL',
+  PAT: 'pat_iKMVZLTMO8zmb9mEBbszJfOgp0ap7LIZbEYuex6u4CA9HewCeA0qj6DaSlrgCoho',
   BOT_ID: '7615218949697126443',
   API_URL: `${COZE_BASE}/v3/chat`,
   BASE: COZE_BASE,
